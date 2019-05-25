4eea3d2dd451a6f24694a5ec339a2259773ac229
Ricardo Calixto <rcalixtos@gmail.com>
2019-05-25 15:37:45 -0300

    App is functional. TODO: JS REFACTOR, CSS

--- calculator.js
+++ calculator.js
@@ -3,19 +3,13 @@ const keys = calculator.querySelector(".calculator__keys");
 const display = document.querySelector(".calculator__display");
 
 const calculate = (n1, operator, n2) => {
-  let result = "";
+  const firstNum = parseFloat(n1);
+  const secondNum = parseFloat(n2);
 
-  if (operator === "add") {
-    result = parseFloat(n1) + parseFloat(n2);
-  } else if (operator === "subtract") {
-    result = parseFloat(n1) - parseFloat(n2);
-  } else if (operator === "multiply") {
-    result = parseFloat(n1) * parseFloat(n2);
-  } else if (operator === "divide") {
-    result = parseFloat(n1) / parseFloat(n2);
-  }
-
-  return result;
+  if (operator === "add") return firstNum + secondNum;
+  if (operator === "subtract") return firstNum - secondNum;
+  if (operator === "multiply") return firstNum * secondNum;
+  if (operator === "divide") return firstNum / secondNum;
 };
 
 keys.addEventListener("click", e => {
@@ -28,14 +22,18 @@ keys.addEventListener("click", e => {
   if (e.target.matches("button")) {
     if (!action) {
       console.log(keyContent);
-      if (displayedNum === "0" || previousKeyType === "operator") {
+      if (
+        displayedNum === "0" ||
+        previousKeyType === "operator" ||
+        previousKeyType === "calculate"
+      ) {
         display.textContent = keyContent;
       } else {
         display.textContent = displayedNum + keyContent;
         console.log(display.textContent);
       }
 
-      calculator.dataset.previousKey = "number";
+      calculator.dataset.previousKeyType = "number";
     }
 
     if (
@@ -44,13 +42,22 @@ keys.addEventListener("click", e => {
       action === "multiply" ||
       action === "divide"
     ) {
-      console.log("Você pressionou uma tecla operacional.");
+      console.log("Você pressionou uma tecla de operação.");
       const firstValue = calculator.dataset.firstValue;
       const operator = calculator.dataset.operator;
       const secondValue = displayedNum;
 
-      if (firstValue && operator && previousKeyType !== "operator") {
-        display.textContent = calculate(firstValue, operator, secondValue);
+      if (
+        firstValue &&
+        operator &&
+        previousKeyType !== "operator" &&
+        previousKeyType !== "calculate"
+      ) {
+        const calcValue = calculate(firstValue, operator, secondValue);
+        display.textContent = calcValue;
+        calculator.dataset.firstValue = calcValue;
+      } else {
+        calculator.dataset.firstValue = displayedNum;
       }
 
       calculator.dataset.previousKeyType = "operator";
@@ -62,31 +69,50 @@ keys.addEventListener("click", e => {
       console.log("Você pressinou o ponto!");
       if (!displayedNum.includes(".")) {
         display.textContent = displayedNum + ".";
-      } else if (previousKeyType === "operator") {
+      } else if (
+        previousKeyType === "operator" ||
+        previousKeyType === "calculate"
+      ) {
         display.textContent = "0.";
       }
 
-      calculator.dataset.previousKey = "decimal";
+      calculator.dataset.previousKeyType = "decimal";
+    }
+    if (action !== "clear") {
+      const clearButton = calculator.querySelector("[data-action=clear]");
+      clearButton.textContent = "CE";
     }
 
     if (action === "clear") {
       console.log("Você pressionou a tecla para dar clear");
-      calculator.dataset.previousKey = "clear";
+
+      if (key.textContent === "AC") {
+        calculate.dataset.firstValue = "";
+        calculate.dataset.modValue = "";
+        calculate.dataset.operator = "";
+        calculate.dataset.previousKeyType = "";
+      } else {
+        key.textContent = "AC";
+      }
+      display.textContent = 0;
+      calculator.dataset.previousKeyType = "clear";
     }
 
     if (action === "calculate") {
       let firstValue = calculator.dataset.firstValue;
       const operator = calculator.dataset.operator;
-      const secondValue = displayedNum;
+      let secondValue = displayedNum;
 
       if (firstValue) {
         if (previousKeyType === "calculate") {
           firstValue = displayedNum;
+          secondValue = calculator.dataset.modValue;
         }
         display.textContent = calculate(firstValue, operator, secondValue);
       }
 
-      calculator.dataset.previousKey = "calculate";
+      calculator.dataset.modValue = secondValue;
+      calculator.dataset.previousKeyType = "calculate";
 
       console.log("Você pressionou o igual!");
     }
